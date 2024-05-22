0678f7629dc9b9fbb7609b7ea53085ef874d9fc7
Henry Skiba <henry.skiba@frgrisk.com>
2024-05-22 09:35:18 +0800

    update ami

--- main.go
+++ main.go
@@ -80,7 +80,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 				MinCount:                          aws.Int32(1),
 				MaxCount:                          aws.Int32(1),
 				EbsOptimized:                      aws.Bool(true),
-				ImageId:                           aws.String("ami-0b5c193aca8cd5099"),
+				ImageId:                           aws.String("ami-07b445f50ba67b694"),
 				InstanceInitiatedShutdownBehavior: types.ShutdownBehaviorTerminate,
 				InstanceType:                      instanceType,
 				SubnetId:                          aws.String("subnet-0c7485057fba6c4f6"),
