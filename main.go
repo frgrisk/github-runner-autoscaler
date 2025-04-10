ece2e5172b9baa613ce2b75b5d2519c2bad44818
Henry Skiba <henry.skiba@frgrisk.com>
2025-04-10 22:54:31 +0800

    add some logging

--- main.go
+++ main.go
@@ -136,6 +136,8 @@ func main() {
 }
 
 var userData = `#!/bin/bash
+set -x
+START_TIME=$(date +%s)
 shutdown +60
 sed -i 's/ap-southeast-3/us-east-2/g' /etc/apt/sources.list
 usermod -aG docker ubuntu
@@ -143,7 +145,7 @@ cd /opt
 mkdir actions-runner
 chown -R ubuntu:ubuntu actions-runner
 cd actions-runner
-sudo -u ubuntu tar xvzf ../runner-cache/actions-runner-linux-* -C .
+sudo -u ubuntu tar xzf ../runner-cache/actions-runner-linux-* -C .
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 INSTANCE_TYPE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type)
 GITHUB_TOKEN=$(curl -L \
@@ -153,6 +155,9 @@ GITHUB_TOKEN=$(curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/orgs/frgrisk/actions/runners/registration-token | jq -r .token)
 sudo -u ubuntu ./config.sh --url https://github.com/frgrisk --token $GITHUB_TOKEN --disableupdate --ephemeral --labels $INSTANCE_TYPE,ephemeral,X64 --unattended
+END_TIME=$(date +%s)
+EXECUTION_TIME=$((END_TIME - START_TIME))
+echo "Script execution time: $EXECUTION_TIME seconds" | tee -a /var/log/setup-time.log
 sudo -u ubuntu ./run.sh
 shutdown now
 `
